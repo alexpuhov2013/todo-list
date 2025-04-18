c94ff5f75aa33d03145e1793d1dae6fcf55daa60
Your Name <you@example.com>
2025-04-18 20:01:32 +0300

    upd

--- script.js
+++ script.js
@@ -6,23 +6,35 @@ const list = document.querySelector("#item-list")
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    if(input.value === ""){
+    if (input.value.trim() === "") {
         alert("Пожалуйста заполните поле")
-    } else{
-        const newItem = input.value
-
-        const li = document.createElement("li")
-        li.appendChild(document.createTextNode(newItem))
-
-        const button = document.createElement('button')
-        button.className = "remove-item btn-link text-red"
-        
-        const icon = document.createElement("i")
-        icon.className = "fa-solid fa-xmark"
-
-        button.appendChild(icon)
-        li.appendChild(button)
-        list.appendChild(li)
+        return;
     }
 
+    const newItem = input.value
+
+    const li = document.createElement("li")
+    li.appendChild(document.createTextNode(newItem))
+
+    const button = createBtn("remove-item btn-link text-red")
+    
+    li.appendChild(button)
+    list.appendChild(li)
+
 })
+
+
+
+function createBtn(classes) {
+    const button = document.createElement('button')
+    button.className = classes
+    const icon = createIcon("fa-solid fa-xmark")
+    button.appendChild(icon)
+    return button;
+}
+
+function createIcon(classes) {
+    const icon = document.createElement("i")
+    icon.className = classes
+    return icon;
+}
